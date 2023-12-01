10f0b1de501c4f11dd7b6eb69a9f0b5599d667e4
ShashiKumar03 <shashikumarkushwaha3@gmail.com>
2023-12-01 12:25:35 +0530

    modificatiom

--- fs-problem1.cjs
+++ fs-problem1.cjs
@@ -1,31 +1,29 @@
 const fs = require("fs");
 function createAndDeleteTheDirectory(mkdirPath, n, callback) {
-  if (n == 0) {
-    console.log("sucessfully all files are deleted");
-    return;
-  }
-
   fs.mkdir(mkdirPath, { recursive: true }, (err) => {
     if (err) {
       callback(err);
       return;
     } else {
-      createJsonFile(`./jsonDirectory/${n}.json`, (err, filePath) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(`filepath is ${filePath} is createdSuccessfully`);
-          deleteFiles(filePath, (err) => {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log(`filePath is ${filePath} is deleted suceesfully`);
-              createAndDeleteTheDirectory(mkdirPath, --n, callback);
-            }
-          });
-        }
-      });
-      callback(null);
+      while (n > 0) {
+        createJsonFile(`./jsonDirectory/${n}.json`, (err, filePath) => {
+          if (err) {
+            console.log(err);
+            callback(err);
+          } else {
+            console.log(`filepath is ${filePath} is createdSuccessfully`);
+            deleteFiles(filePath, (err) => {
+              if (err) {
+                console.log(err);
+                callback(err);
+              } else {
+                console.log(`filePath is ${filePath} is deleted suceesfully`);
+              }
+            });
+          }
+        });
+        n--;
+      }
     }
   });
 }
